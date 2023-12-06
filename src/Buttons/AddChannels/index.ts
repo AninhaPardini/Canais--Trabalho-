a52f4fbc16dc2e6da3618c0a61c2bce7b4eb4684
AninhaPardini <aninhapardini.work@gmail.com>
2023-12-06 14:12:19 -0300

    Alteração de arquitetura e impotação

--- src/Buttons/AddChannels/index.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Telegraf } from "telegraf";
-
-const addChannels = async (bot: Telegraf) => {
-  bot.action('ADICIONAR CANAL', async (ctx: Context) => {
-  });
-}
-
-export default addChannels;
--- src/Messages/channelsList.message.ts
+++
@@ -1,9 +0,0 @@
-import * as Telegraf from "telegraf";
-
-const channelListMessage = async (bot) => {
-  await ctx.deleteMessage();
-
-  await bot.reply((ctx) => ctx.reply(''))
-}
-
-export default channelListMessage;
